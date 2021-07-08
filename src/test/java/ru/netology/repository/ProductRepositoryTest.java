8c0d10f98d4684b1b203e066ad5f165d20cd2b9e
Ekaterina5885 <82658524+Ekaterina5885@users.noreply.github.com>
2021-07-08 17:00:35 +0300

    Update ProductRepositoryTest.java

--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -44,12 +44,8 @@ public class ProductRepositoryTest {
     @Test
     public void shouldRemoveByIdNonexistentElement() {
 
-        try {
             repository.removeById(8);
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        }
-
+      
         assertThrows(NotFoundException.class, () -> {
             repository.removeById(8);
         });
